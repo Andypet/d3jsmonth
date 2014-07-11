1c7640fe4e7c488b802fd3cba56d95565ecb7f7e
kingtak <k.just.wong@gmail.com>
2014-07-10 19:31:05 -1000

    got rid of old waypoint and added jquery waypoints, fixed up the first part

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -4,6 +4,7 @@ $(document).ready(function(){
   var height = 0;
   var map = true;
   var hawaiiSvg = d3.select(".hawaiiMap").append("svg");
+
   //add in ajax class to a menu
   $('a.hijax').click(function (e) {
     e.preventDefault(); // Stop browser from loading the URL.
@@ -14,76 +15,67 @@ $(document).ready(function(){
     });
   });
 
+
+  //options for skrollr interface
   var s = skrollr.init({
-    forceHeight: false,
-    render: function(data) {
-      //Debugging - Log the current scroll position.
-      if(data.curTop >= "100" && map == true){
-        map = false;
-        drawingHawaii();
-      }
-    }
+    forceHeight: false
   });
 
-  function drawingHawaii(){
+  $('#slide-2').waypoint(function(){
+    drawingHawaii();
+    $(this).waypoint('destroy');
+  });
 
+
+  function drawingHawaii(){
+    console.log("using it");
     var map = $('.hawaiiMap');
     width = map[0].offsetWidth;
     height = 500;
     
-    console.log(width);
-    console.log(height);
     hawaiiSvg.attr("width", width).attr("height", height);
-    queue().defer(d3.json, "/data/hawaii_topojson.json")
-           .await(ready);
+    d3.json( "/data/hawaii_topojson.json", function(error, hawaii){
 
-  }
-
-  function ready(error, hawaii){
-    drawMap(hawaii);
-  }
-
-  function drawMap(hawaii){
-
-    var subunits = topojson.feature(hawaii, hawaii.objects.subunits);
+      var subunits = topojson.feature(hawaii, hawaii.objects.subunits);
     
-    // Hawaii: 19.5667° N, 155.5000° W
-    // All of Hawaii's Islands:
-    // var projection = d3.geo.albers()
-    //   .parallels([15, 25])
-    var projection = d3.geo.albers()
-    .rotate([157.50, 0])
-    .center([0, 18.5])
-    // .origin([-160, 20])
-    .parallels([15, 25])
-    .scale(6000)
-    .translate([width/2 , height ]);
+      // Hawaii: 19.5667° N, 155.5000° W
+      // All of Hawaii's Islands:
+      // var projection = d3.geo.albers()
+      //   .parallels([15, 25])
+      var projection = d3.geo.albers()
+      .rotate([157.50, 0])
+      .center([0, 18.5])
+      // .origin([-160, 20])
+      .parallels([15, 25])
+      .scale(6000)
+      .translate([width/2 , height ]);
 
-    var path = d3.geo.path()
-      .projection(projection);
+      var path = d3.geo.path()
+        .projection(projection);
 
-    hawaiiSvg.append("path")
-      .datum(subunits)
-      .attr("class", "state")
-      .attr("d", path);
+      hawaiiSvg.append("path")
+        .datum(subunits)
+        .attr("class", "state")
+        .attr("d", path);
 
+
+    });
+  }
+
+  function drawUsa(){
 
   }
 
+  $(window).on('resize', resize);
 
+  function resize() {
+    var map = $('.hawaiiMap').width();
 
-
-$(window).on('resize', resize);
-
-function resize() {
-  var map = $('.hawaiiMap').width();
-
-  // adjust things when the window size changes
-  width = map;
-  // update projection
-  hawaiiSvg.attr("width", width);
-}
-
+    // adjust things when the window size changes
+    width = map;
+    // update projection
+    hawaiiSvg.attr("width", width);
+  }
 
 
 
